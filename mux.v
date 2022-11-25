5f88dcdc9c0de77ae010453ba3c309b4236326a1
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2022-11-24 23:26:15 -0600

    mux file formatting
    added intents for readability

--- mux.v
+++ mux.v
@@ -1,21 +1,21 @@
 module mux_4b (a, b, sel, out);
-
-input wire [3:0] a;
-input wire [3:0] b;
-input wire sel;
-output wire [3:0] out;
-
-assign out = (sel) ? b : a;
-
+	
+	input wire [3:0] a;
+	input wire [3:0] b;
+	input wire sel;
+	output wire [3:0] out;
+	
+	assign out = (sel) ? b : a;
+	
 endmodule
 
 module mux_32b (a, b, sel, out);
 
-input wire [31:0] a;
-input wire [31:0] b;
-input wire sel;
-output wire [31:0] out;
+	input wire [31:0] a;
+	input wire [31:0] b;
+	input wire sel;
+	output wire [31:0] out;
 
-assign out = (sel) ? b : a;
+	assign out = (sel) ? b : a;
 
 endmodule
