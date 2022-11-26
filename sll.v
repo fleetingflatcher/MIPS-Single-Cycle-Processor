afe62d557755f780eab8a432e03821967fd28db9
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2022-11-26 01:12:38 -0600

    Update sll.v
    Updated syntax

--- sll.v
+++ sll.v
@@ -1,9 +1,6 @@
 module sll_16b (operand, result);
-	input [15:0] operand;
-	output [15:0] result;
-	
-	wire [15:0] operand;
-	wire [15:0] result;
+	input wire [15:0] operand;
+	output wire [15:0] result;
 	
 	assign result [15:1] 	= operand[14:0];
 	assign result [0] 		= 0;
