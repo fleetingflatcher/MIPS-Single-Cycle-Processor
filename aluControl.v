e6fec83e00f7d072b550bbb98d6312f987630b3d
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2022-12-01 21:48:08 -0600

    Update aluControl.v
    Switched to actual MIPS ALU op codes.

--- aluControl.v
+++ aluControl.v
@@ -10,24 +10,24 @@ module aluControl (aluInstruct, instructFunc, aluOperationCode);
 		case (aluInstruct)
 			3'b00:
 			begin
-				aluOperationCode <= 3'b000;
+				aluOperationCode <= 3'b010;
 			end
 			3'b01:
 			begin
-				aluOperationCode <= 3'b001;
+				aluOperationCode <= 3'b110;
 			end
 			3'b10:
 			begin
 				if (instructFunc == 6'b100000) 
-					aluOperationCode <= 3'b000;
+					aluOperationCode <= 3'b010;
 				else if (instructFunc == 6'b100010) 
-					aluOperationCode <= 3'b001;
+					aluOperationCode <= 3'b110;
 				else 
 					aluOperationCode <= 3'bZZZ;
 			end
 			3'b11:
 			begin
-				aluOperationCode <= 3'b000;
+				aluOperationCode <= 3'b010;
 			end
 		endcase
 	end
