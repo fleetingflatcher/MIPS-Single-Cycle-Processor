de470de767ff7d207c2c5bc09845fbbd49957312
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2022-12-01 20:54:15 -0600

    Update pc.v

--- pc.v
+++ pc.v
@@ -1,7 +1,8 @@
-module programCounter (CLK, write, read);
+module programCounter (CLK, write, reset, read);
 
 input wire CLK;
 input wire [31:0] write;
+input reset;
 
 output reg [31:0] read;
 
@@ -10,6 +11,9 @@ begin
 	if (enable) read <= write;
 	else read <= read;
 end
-
+always @ (posedge reset)
+begin
+	if (reset) read = 32'h0000_0000;
+end
 
 endmodule
