62e1d012872c84a29d7a536a1ab0b55510ee347d
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2022-12-01 21:47:38 -0600

    Update instructionMemory.v
    Fixed endian-ness in the instruction memory and output mapping.

--- instructionMemory.v
+++ instructionMemory.v
@@ -1,18 +1,27 @@
 module instructionMemory (address, currentInstruction);
 
-	input wire [5:0] address;
+	input wire [4:0] address;
 	output reg [31:0] currentInstruction;
 	
-	reg [7:0] mem [127:0];
+	reg [7:0] mem [255:0];
 	
 	initial
 	begin
-		mem[ 0] = 8'h21;	mem[ 1] = 8'h08;	mem[ 2] = 8'h00;	mem[ 3] = 8'h02; // addi $t2 $t2 2 
-		mem[ 4] = 8'h01;	mem[ 5] = 8'h0a;	mem[ 6] = 8'h40;	mem[ 7] = 8'h20; // add  $t0 $t0 $t2
-		mem[ 8] = 8'had;	mem[ 9] = 8'h28;	mem[10] = 8'h00;	mem[11] = 8'h00; // sw   $t0 0($t1)
-		mem[12] = 8'h08;	mem[13] = 8'h00;	mem[14] = 8'h00;	mem[15] = 8'h01; // j to iMem position 4?
+		mem[ 3] = 8'h21;	mem[ 2] = 8'h08;	mem[ 1] = 8'h00;	mem[ 0] = 8'h02; // addi $t2 $t2 2 
+		mem[ 7] = 8'h01;	mem[ 6] = 8'h0a;	mem[ 5] = 8'h40;	mem[ 4] = 8'h20; // add  $t0 $t0 $t2
+		mem[11] = 8'had;	mem[10] = 8'h28;	mem[ 9] = 8'h00;	mem[ 8] = 8'h00; // sw   $t0 0($t1)
+		mem[15] = 8'h08;	mem[14] = 8'h00;	mem[13] = 8'h00;	mem[12] = 8'h01; // j to iMem position 4?
 	end
 
-	always@* currentInstruction = mem[address];
+	always @ (currentInstruction or address) 
+	begin
+		if (address == 5'bXXXXX) currentInstruction <= 32'hZZZZ_ZZZZ;
+		else begin 
+			currentInstruction[31:24] <= mem[address+3];
+			currentInstruction[23:16] <= mem[address+2];
+			currentInstruction[15: 8] <= mem[address+1];
+			currentInstruction[ 7: 0] <= mem[address  ];
+		end
+	end
 
 endmodule
