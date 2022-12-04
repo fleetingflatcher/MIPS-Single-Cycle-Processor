4ea99575da0b155911bfcc3a60bea1fd33403cb2
fleetingflatcher <43767847+fleetingflatcher@users.noreply.github.com>
2022-12-03 21:07:49 -0600

    Different bit adders
    Added a 5-bit adder in addition to the 32-bit adder.

--- adder.v
+++ adder.v
@@ -1,9 +1,20 @@
-module adder (operand1, operand2, result);
+module adder_5b (operand1, operand2, result);
 	
+	input wire [4:0] operand1;
+	input wire [4:0] operand2;
+	output wire [4:0] result;
+	
+	assign result = operand1 + operand2;
+
+endmodule
+
+
+module adder_32b (operand1, operand2, result);
+
 	input wire [31:0] operand1;
 	input wire [31:0] operand2;
 	output wire [31:0] result;
-	
+
 	assign result = operand1 + operand2;
-	
+
 endmodule
